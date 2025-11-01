eae579877f00ecf9f692e7b08e2e6fb4e7acb9e3
Marcel Gaub <mgaub@smail.uni-koeln.de>
2025-11-01 20:01:44 +0100

    Package p1 Würfel

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -1,4 +1,26 @@
 package p1;
 
+import java.util.Random;
+
 public class P1_main {
+    public static void main(String[] args) {
+
+        // Erzeuge ein Zufallsobjekt
+        Random zufall = new Random();
+
+        // Generiere eine Zahl von 1 bis 6 (inklusive)
+        int wuerfel = zufall.nextInt(6) + 1;
+
+        System.out.println("Gewürfelte Zahl: " + wuerfel);
+
+        if (wuerfel == 1 || wuerfel == 4) {
+            System.out.println("Gewonnen!");
+        } else if (wuerfel == 2) {
+            System.out.println("Verloren!");
+        } else if (wuerfel == 5) {
+            System.out.println("Unentschieden.");
+        } else {
+            System.out.println("Lage unklar.");
+        }
+    }
 }
